4576b514bbfb380ee607826eedac96003e84ec78
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-02 10:40:03 +0300

    fizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,7 +1,8 @@
 const fizzBuzz = (n) => {
-
-  for (let i = 0; i < n; i++) {
-    if (i % 3 == 0) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
       console.log("fizz");
     } else if (i % 5 == 0) {
       console.log("buzz");

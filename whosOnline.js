569254043a88e03f34422ec3bcce0aed131f4ba2
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-09 18:09:38 +0300

    W05D03 problem

---
+++ whosOnline.js
@@ -0,0 +1,25 @@
+const whosOnline = (friends) => {
+  let result = {
+    online: [],
+    offline: [],
+    away: [],
+  };
+  
+  friends.forEach((friend) => {
+    if (friend.status === "offline") {
+      result.offline.push(friend.username);
+    } else if (friend.status === "online") {
+      if (friend.lastActivity > 10) {
+        result.away.push(friend.username);
+      } else {
+        result.online.push(friend.username);
+      }
+    }
+  });
+
+  if (result.online.length == 0) delete result.online;
+  if (result.offline == 0) delete result.offline;
+  if (result.away == 0) delete result.away;
+
+  return result;
+};

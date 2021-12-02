b5103f3b6a72541d422ccf405ca53f154661a35b
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-02 16:37:13 +0300

    first task

---
+++ W08D05.js
@@ -0,0 +1,162 @@
+function isLanguageDiverse(list) {
+  const result = list.reduce(
+    (acc, item) => {
+      if (item.language === "Python") acc.Python++;
+      if (item.language === "Ruby") acc.Ruby++;
+      if (item.language === "JavaScript") acc.JavaScript++;
+
+      return acc;
+    },
+    { Python: 0, Ruby: 0, JavaScript: 0 }
+  );
+
+  if (
+    result.JavaScript / result.Python > 2 ||
+    result.Python / result.JavaScript > 2
+  )
+    return false;
+
+  if (
+    result.JavaScript / result.Ruby > 2 ||
+    result.Ruby / result.JavaScript > 2
+  )
+    return false;
+
+  if (result.Python / result.Ruby > 2 || result.Ruby / result.Python > 2)
+    return false;
+
+  return true;
+}
+
+list1 = [
+  {
+    firstName: "Daniel",
+    lastName: "J.",
+    country: "Aruba",
+    continent: "Americas",
+    age: 42,
+    language: "Python",
+  },
+  {
+    firstName: "Kseniya",
+    lastName: "T.",
+    country: "Belarus",
+    continent: "Europe",
+    age: 22,
+    language: "Ruby",
+  },
+  {
+    firstName: "Sou",
+    lastName: "B.",
+    country: "Japan",
+    continent: "Asia",
+    age: 43,
+    language: "Ruby",
+  },
+  {
+    firstName: "Hanna",
+    lastName: "L.",
+    country: "Hungary",
+    continent: "Europe",
+    age: 95,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Jayden",
+    lastName: "P.",
+    country: "Jamaica",
+    continent: "Americas",
+    age: 18,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Joao",
+    lastName: "D.",
+    country: "Portugal",
+    continent: "Europe",
+    age: 25,
+    language: "JavaScript",
+  },
+];
+
+var list2 = [
+  {
+    firstName: "Daniel",
+    lastName: "J.",
+    country: "Aruba",
+    continent: "Americas",
+    age: 42,
+    language: "Python",
+  },
+  {
+    firstName: "Kseniya",
+    lastName: "T.",
+    country: "Belarus",
+    continent: "Europe",
+    age: 22,
+    language: "Ruby",
+  },
+  {
+    firstName: "Hanna",
+    lastName: "L.",
+    country: "Hungary",
+    continent: "Europe",
+    age: 95,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Jayden",
+    lastName: "P.",
+    country: "Jamaica",
+    continent: "Americas",
+    age: 18,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Joao",
+    lastName: "D.",
+    country: "Portugal",
+    continent: "Europe",
+    age: 25,
+    language: "JavaScript",
+  },
+];
+
+var list3 = [
+  {
+    firstName: "Daniel",
+    lastName: "J.",
+    country: "Aruba",
+    continent: "Americas",
+    age: 42,
+    language: "Python",
+  },
+  {
+    firstName: "Kseniya",
+    lastName: "T.",
+    country: "Belarus",
+    continent: "Europe",
+    age: 22,
+    language: "Ruby",
+  },
+  {
+    firstName: "Jayden",
+    lastName: "P.",
+    country: "Jamaica",
+    continent: "Americas",
+    age: 18,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Joao",
+    lastName: "D.",
+    country: "Portugal",
+    continent: "Europe",
+    age: 25,
+    language: "JavaScript",
+  },
+];
+
+console.log(isLanguageDiverse(list1));
+console.log(isLanguageDiverse(list2));
+console.log(isLanguageDiverse(list3));
